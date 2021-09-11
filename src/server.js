1a716ff78cb54a69a678c26023bf1b65216963e8
thanhbxdev <thanhbxdev@gmail.com>
2021-09-11 10:30:32 +0700

    Deploy thanhbxdev API Heroku

--- src/server.js
+++ src/server.js
@@ -1,6 +1,5 @@
 import express from 'express'
 import { connectDB } from './config/mongodb'
-import { env } from './config/environtment'
 import { apiV1 } from './routes/v1'
 import cors from 'cors'
 import { corsOptions } from './config/cors'
@@ -24,7 +23,11 @@ const bootServer = () => {
   // Use API
   app.use('/v1', apiV1)
 
-  app.listen( env.APP_PORT, env.APP_HOST, () => {
-    console.log(`hello thanhbxdev,Im running at ${env.APP_HOST}:${env.APP_PORT}`)
+  // app.listen( env.APP_PORT, env.APP_HOST, () => {
+  //   console.log(`hello thanhbxdev,Im running at ${env.APP_HOST}:${env.APP_PORT}`)
+  // })
+  //   support heroku deploy
+  app.listen( process.env.PORT, () => {
+    console.log(`hello thanhbxdev,Im running at port ${process.env.PORT}`)
   })
 }
