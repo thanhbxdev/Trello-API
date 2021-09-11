6efa5021591f177d0512f9bfc12098c79d14bed0
thanhbxdev <thanhbxdev@gmail.com>
2021-09-11 10:22:13 +0700

    Deploy thanhbxdev API Heroku

--- src/untilities/const.js
+++ src/untilities/const.js
@@ -5,4 +5,4 @@ export const HttpStatusCode = {
   NOT_FOUND:404,
   INTERNAL_SERVER:500
 }
-export const WHITELIST_DOMAIN = ['http://localhost:3000', 'http://localhost:3001' ]
+export const WHITELIST_DOMAIN = ['http://localhost:3000', 'https://trello-app-clone-c5507.web.app' ]
